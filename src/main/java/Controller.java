8fa2ee192c88afd118a31bf4ad1eb3e0bd44ad44
cyziek <krzysztof112997@gmail.com>
2021-03-04 12:26:36 +0100

    DZIALA!!111111

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -5,7 +5,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.media.Media;
+import javafx.scene.layout.GridPane;
+import javafx.scene.media.*;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import org.hibernate.HibernateException;
@@ -13,6 +14,10 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import uk.co.caprica.vlcj.factory.MediaPlayerFactory;
+import uk.co.caprica.vlcj.media.MediaRef;
+import uk.co.caprica.vlcj.media.TrackType;
+import uk.co.caprica.vlcj.player.base.MediaPlayerEventAdapter;
+import uk.co.caprica.vlcj.player.base.MediaPlayerEventListener;
 import uk.co.caprica.vlcj.player.component.EmbeddedMediaPlayerComponent;
 import uk.co.caprica.vlcj.player.embedded.EmbeddedMediaPlayer;
 
@@ -28,14 +33,16 @@ import java.util.Random;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
-    @FXML
-    private ImageView ImageView;
-    private MediaPlayerFactory mediaPlayerFactory;
-    private EmbeddedMediaPlayer player;
-    private EmbeddedMediaPlayerComponent component;
+//    @FXML
+//    private ImageView ImageView;
+//    private MediaPlayerFactory mediaPlayerFactory;
+//    private EmbeddedMediaPlayer player;
+//    private EmbeddedMediaPlayerComponent component;
     private MediaPlayer mp;
     private Media me;
     private Session session;
+    @FXML
+    private MediaView mediaView;
     @FXML
     private Label TrescPytaniaLabel;
     @FXML
@@ -47,7 +54,14 @@ public class Controller implements Initializable {
     @FXML
     private Button startButton;
     @FXML
-     private Button checkButton;
+    private Button checkButton;
+    @FXML
+    private GridPane GridPane;
+    @FXML
+    private ImageView ImageView;
+    @FXML
+    private Button nextButton;
+
     int selectedanswer = -1;
     String corrAnswer;
     String mediapath;
@@ -75,14 +89,25 @@ public class Controller implements Initializable {
                 Question question = (Question) o;
                 PopulateLabel(question.getContent(), question.getAns1(), question.getAns2(), question.getAns3());
                 corrAnswer = question.getCorrect();
+                mediapath = "D:\\Multimedia_MP4\\" + question.getMedia();
+                nextButton.setDisable(true);
                 if (question.getMedia() == null) {
-                    mediapath = "D:\\Multimedia_KieroTest\\nophoto.jpg";
-                } else {
-                    mediapath = "D:\\Multimedia_KieroTest\\" + question.getMedia();
+                    mediapath = "D:\\Multimedia_MP4\\nophoto.jpg";
+                    System.out.println("1");
+                    startButton.setDisable(true);
+                    ImageDisplay();
+
                 }
-                if (mediapath.contains(".jpg")) {
-                    MediaButton();
+                if (mediapath.contains(".jpg") || mediapath.contains(" .jpg")) {
+                    startButton.setDisable(true);
+                    System.out.println("2");
+                    ImageDisplay();
                 }
+               else {
+                  //  mediapath = "D:\\Multimedia_MP4\\" + question.getMedia();
+                    startButton.setDisable(false);
+                }
+
                 System.out.println("Content: " + question.getContent());
                 System.out.println("Odp1: " + question.getAns1());
                 System.out.println("Odp2: " + question.getAns2());
@@ -99,17 +124,25 @@ public class Controller implements Initializable {
         }
     }
 
+
     @FXML
     private void MediaButton() {
-        mediaPlayerFactory = new MediaPlayerFactory();
-        player = mediaPlayerFactory.mediaPlayers().newEmbeddedMediaPlayer();
-        player.videoSurface().set(videoSurfaceForImageView(ImageView));
-        player.media().play(mediapath);
-        System.out.println("Media w miejscu odpalania: " + mediapath);
-
+        System.out.println("media button function ");
+        System.out.println("Mediapath: " + mediapath);
+        String path = new File(mediapath).getAbsolutePath();
+        me = new Media(new File(path).toURI().toString());
+        mp = new MediaPlayer(me);
+        mediaView.setMediaPlayer(mp);
+        mp.setAutoPlay(true);
         startButton.setDisable(true);
 
+    }
 
+    public void ImageDisplay(){
+        File file = new File(mediapath);
+        Image image = new Image(file.toURI().toString());
+        ImageView.setImage(image);
+        startButton.setDisable(true);
     }
 
     public void PopulateLabel(String content, String odp1, String odp2, String odp3) {
@@ -145,6 +178,7 @@ public class Controller implements Initializable {
 
     @FXML
     public void checkButtonAction() {
+        nextButton.setDisable(false);
         if (selectedanswer == -1) {
             System.out.println("Nie wybrano żadnej odpowiedzi!");
             correctLabel().setStyle("-fx-background-color: #007700");
@@ -190,18 +224,20 @@ public class Controller implements Initializable {
 
     @FXML
     public void nextButton() {
-        LoadQuestion();
+
         selectedanswer = -1;
         odp1Label.setStyle(null);
         odp2Label.setStyle(null);
         odp3Label.setStyle(null);
-        ImageView.setImage(null);
+//        ImageView.setImage(null);
         startButton.setDisable(false);
         checkButton.setDisable(true);
         odp3Label.setVisible(true);
         odp3Label.setText(null);
-        mediaPlayerFactory=null;
-        player=null;
+        ImageView.setImage(null);
+        mediaView.setMediaPlayer(null);
+        LoadQuestion();
+
     }
 
 
